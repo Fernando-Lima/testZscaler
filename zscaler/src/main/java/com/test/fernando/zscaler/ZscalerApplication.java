ca65708c68e74b6dfa7d66091e11f2347f4ad664
Fernando Lima <fernando.lima@eccreguladora.com.br>
2024-12-10 16:51:23 -0300

    fisrt commit

---
+++ zscaler/src/main/java/com/test/fernando/zscaler/ZscalerApplication.java
@@ -0,0 +1,13 @@
+package com.test.fernando.zscaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZscalerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZscalerApplication.class, args);
+	}
+
+}
